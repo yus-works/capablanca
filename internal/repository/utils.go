3b9936c9af49fcc343b3d8a9741c21500df68b69
teodord25 <djuric.teodor25@gmail.com>
2025-02-28 21:18:19 +0100

    test

--- internal/repository/utils.go
+++ internal/repository/utils.go
@@ -6,6 +6,7 @@ import (
 )
 
 // TODO: cache metadata queries somehow
+// TODO: test
 
 // GetTableNames returns a list of all table names in the database.
 func GetTableNames(db *gorm.DB) ([]string, error) {
