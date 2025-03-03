dc412c8f4e982521b70e9dfd0882075da57db973
teodord25 <djuric.teodor25@gmail.com>
2025-03-03 22:08:25 +0100

    refactor table handler

--- internal/routing/handlers.go
+++ internal/routing/handlers.go
@@ -1,11 +1,10 @@
 package routing
 
 import (
-	"strings"
-
 	"github.com/labstack/echo/v4"
 	"github.com/yus-works/capablanca/internal/repository"
 	"github.com/yus-works/capablanca/internal/templates"
+	"github.com/yus-works/capablanca/internal/utils"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
@@ -19,21 +18,18 @@ func RegisterRoutes(e *echo.Echo, log *zap.Logger, db *gorm.DB) {
 	e.GET("/table/:name", func(c echo.Context) error {
 		log.Debug("RENDER: root endpoint")
 
-		names, err := repository.GetTableNames(db)
+		namesSnake, err := repository.GetTableNames(db)
 		if err != nil {
 			log.Error("Failed to get table names")
 		}
 
-		var namesClean []string
-		for _, name := range names {
-			if strings.Contains(name, "_") {
+		namesPascal := make([]string, len(namesSnake))
+		for i, name := range namesSnake {
+			if len(name) == 0 {
 				continue
 			}
 
-			if len(name) > 0 {
-				title := strings.ToUpper(name[:1]) + name[1:]
-				namesClean = append(namesClean, title)
-			}
+			namesPascal[i] = utils.SnakeToPascal(name)
 		}
 
 		tableName := c.Param("name")
@@ -43,7 +39,7 @@ func RegisterRoutes(e *echo.Echo, log *zap.Logger, db *gorm.DB) {
 			log.Error("Failed to get table data and metadata")
 		}
 
-		content := templates.Table(namesClean, table)
+		content := templates.Table(namesPascal, table)
 
 		return HTML(c, templates.Base("my title", content), 200)
 	})
